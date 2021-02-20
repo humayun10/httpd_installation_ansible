29d0f4db5b3888f8380f5d429dc1f98f2c19a46a
humayun10 <humayunalam@hotmail.com>
2021-02-20 04:04:52 +0000

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -6,7 +6,7 @@ userdel -r halam
 groupdel admin
 rm -rf /var/www/html/test*
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^PermitRootLogin no/PermitRootLogin yes/' /etc/ssh//sshd_config
+sed -i 's/^PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i '/^%admin ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers
 rm -f /etc/sudoers.d/halam
 systemctl restart sshd
